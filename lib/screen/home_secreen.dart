03969fad7e368d51e4533e6813ae557f779f3a35
jaesung <ppaccomy@gmail.com>
2023-08-21 13:24:17 +0900

    230821 |  섹션7-6 StreamProvider

--- lib/screen/home_secreen.dart
+++ lib/screen/home_secreen.dart
@@ -3,6 +3,7 @@ import 'package:rd/layout/default_layout.dart';
 import 'package:rd/screen/future_provider_screen.dart';
 import 'package:rd/screen/state_notifier_provider_screen.dart';
 import 'package:rd/screen/state_provider_screen.dart';
+import 'package:rd/screen/stream_provider_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -45,7 +46,18 @@ class HomeScreen extends StatelessWidget {
               );
             },
             child: Text('FutureProviderScreen'),
-          )
+          ),
+          SizedBox(height: 16.0),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => StreamProviderScreen(),
+                ),
+              );
+            },
+            child: Text('StreamProviderScreen'),
+          ),
         ],
       ),
     );
