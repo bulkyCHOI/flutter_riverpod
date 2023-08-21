2847d4d5670af136a0d2b2abaa8d9db6daab757a
jaesung <ppaccomy@gmail.com>
2023-08-21 18:06:38 +0900

    230821 |  섹션7-11 Provider안에 Provider 사용하기

--- lib/screen/home_secreen.dart
+++ lib/screen/home_secreen.dart
@@ -4,6 +4,7 @@ import 'package:rd/screen/auto_dispose_modifier_screen.dart';
 import 'package:rd/screen/family_modifier_screen.dart';
 import 'package:rd/screen/future_provider_screen.dart';
 import 'package:rd/screen/listen_provider_screen.dart';
+import 'package:rd/screen/provider_screen.dart';
 import 'package:rd/screen/select_provider_screen.dart';
 import 'package:rd/screen/state_notifier_provider_screen.dart';
 import 'package:rd/screen/state_provider_screen.dart';
@@ -105,6 +106,17 @@ class HomeScreen extends StatelessWidget {
               );
             },
             child: Text('SelectProviderScreen'),
+          ),
+          SizedBox(height: 16.0),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => ProviderScreen(),
+                ),
+              );
+            },
+            child: Text('ProviderScreen'),
           ),
         ],
       ),
