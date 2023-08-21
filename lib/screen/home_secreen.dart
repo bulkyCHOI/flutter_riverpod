500a08712711d8600f0126799093861e90092e7e
jaesung <ppaccomy@gmail.com>
2023-08-21 16:23:33 +0900

    230821 |  섹션7-9 Listen 함수 실습

--- lib/screen/home_secreen.dart
+++ lib/screen/home_secreen.dart
@@ -3,6 +3,7 @@ import 'package:rd/layout/default_layout.dart';
 import 'package:rd/screen/auto_dispose_modifier_screen.dart';
 import 'package:rd/screen/family_modifier_screen.dart';
 import 'package:rd/screen/future_provider_screen.dart';
+import 'package:rd/screen/listen_provider_screen.dart';
 import 'package:rd/screen/state_notifier_provider_screen.dart';
 import 'package:rd/screen/state_provider_screen.dart';
 import 'package:rd/screen/stream_provider_screen.dart';
@@ -81,6 +82,17 @@ class HomeScreen extends StatelessWidget {
               );
             },
             child: Text('AutoDisposeModifierScreen'),
+          ),
+          SizedBox(height: 16.0),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => ListenProviderScreen(),
+                ),
+              );
+            },
+            child: Text('ListenProviderScreen'),
           ),
         ],
       ),
