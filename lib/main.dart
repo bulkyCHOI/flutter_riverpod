3b9d8794d24f0e6ae00a1f591d46fdbaa655b6c9
jaesung <ppaccomy@gmail.com>
2023-08-22 13:39:37 +0900

    230822 |  섹션7 완료

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:rd/riverpod/provider_observer.dart';
 import 'package:rd/screen/home_secreen.dart';
 
 void main() {
   runApp(
     ProviderScope(  // MaterialApp 위에 ProviderScope이 있어야 riverpod을 쓸수 있다.
+      observers: [
+        Logger(),
+      ],
       child: MaterialApp(
         home: HomeScreen(),
       ),
